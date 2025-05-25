cfc649a47e680a4960d2516cf2675bd7ec25a2c2
attaurrehman01 <attaurrehman031@gmail.com>
2025-05-25 10:16:46 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Posts from './pages/Posts';
+import Comments from './pages/Comments';
+import Albums from './pages/Albums';
+import Todos from './pages/Todos';
+import Users from './pages/Users';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-container">
+        <Sidebar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Navigate to="/posts" />} />
+            <Route path="/posts" element={<Posts />} />
+            <Route path="/comments" element={<Comments />} />
+            <Route path="/albums" element={<Albums />} />
+            <Route path="/todos" element={<Todos />} />
+            <Route path="/users" element={<Users />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
